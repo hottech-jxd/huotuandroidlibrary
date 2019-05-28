08f8d03a8b72abe1147a672a3b2eb48685a6a54a
hottech-jxd <51818549@qq.com>
2019-05-28 15:53:19 +0800

    更新版本

--- libpay/src/main/java/com/huotu/android/library/libpay/EncryptUtil.java
+++ libpay/src/main/java/com/huotu/android/library/libpay/EncryptUtil.java
@@ -26,7 +26,8 @@ public class EncryptUtil {
                 MessageDigest messageDigest = MessageDigest.getInstance("MD5");
                 messageDigest.update(source.getBytes("utf-8"));
                 byte[] s1 = messageDigest.digest();
-                String tem = new String( Hex.encodeHex(s1) );
+                //String tem = new String( Hex.encodeHex(s1) );
+                String tem = new String( encodeHex(s1) );
                 //Log.i("test>>>>>>>>", tem);
                 return tem;
             }catch (UnsupportedEncodingException ex){
@@ -36,4 +37,23 @@ public class EncryptUtil {
             }
         }
     }
+
+
+    protected static char[] encodeHex(final byte[] data) {
+        final int l = data.length;
+        final char[] out = new char[l << 1];
+        // two characters form the hex value.
+        for (int i = 0, j = 0; i < l; i++) {
+            out[j++] = DIGITS_LOWER[(0xF0 & data[i]) >>> 4];
+            out[j++] = DIGITS_LOWER[0x0F & data[i]];
+        }
+        return out;
+    }
+
+    /**
+     * Used to build output as Hex
+     */
+    private static final char[] DIGITS_LOWER = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd','e', 'f'};
+
+
 }
